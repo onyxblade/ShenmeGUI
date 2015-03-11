0c92e52556130bd424c9b477ef99bb4e73e28998
Cichol <cichol@live.cn>
2015-03-11 11:53:38 +0800

    add select

--- lib/shenmegui/core.rb
+++ lib/shenmegui/core.rb
@@ -1,5 +1,44 @@
 module ShenmeGUI
 
+  class HookedArray < Array 
+    
+    @unhook_methods = %i{<< []= clear collect! compact! concat delete delete_at delete_if fill flatten! replace insert keep_if map map! pop push reject! replace rotate! select! shift shuffle! slice! sort! sort_by! uniq! unshift}
+    @unhook_methods = Hash[@unhook_methods.collect{|x| [x, Array.instance_method(x)]}]
+
+    def initialize(arr, owner)
+      @owner = owner
+      super(arr)
+    end
+    
+    @unhook_methods.each do |k, v|
+      define_method(k) do |*arr, &block|
+        result = v.bind(self).call(*arr, &block)
+        @owner.sync
+        result
+      end
+    end
+
+  end
+
+  class HookedString < String
+    @unhook_methods = %i{<< []= capitalize! chomp! chop! clear concat delete! downcase! encode! force_encoding gsub! insert lstrip! succ! next! prepend replace reverse! rstrip! slice! squeeze! strip! sub! swapcase! tr! tr_s! upcase!}
+    @unhook_methods = Hash[@unhook_methods.collect{|x| [x, String.instance_method(x)]}]
+    
+    def initialize(str, owner)
+      @owner = owner
+      super(str)
+    end
+
+    @unhook_methods.each do |k, v|
+      define_method(k) do |*arr, &block|
+        result = v.bind(self).call(*arr, &block)
+        @owner.sync
+        result
+      end
+    end
+
+  end
+  
   class << self
     attr_accessor :elements, :socket
     attr_reader :this
@@ -14,7 +53,7 @@ module ShenmeGUI
           obj
       end
     end
-      
+
     def handle(msg)
       match_data = msg.match(/(.+?):(\d+)(?:->)?({.+?})?/)
       command = match_data[1].to_sym
